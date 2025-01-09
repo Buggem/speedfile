69591b27df58b6f8b1063c344765a612a982bda6
Max Parry <66670928+Buggem@users.noreply.github.com>
2025-01-09 21:26:56 +1100

    Update send.sh

--- send.sh
+++ send.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-if [ $# -lt 2 ];
+if [ $# -ne 3 ];
 then
-    	echo "Usage: ./send.sh [input filename] [ip address]"
+    	echo "Usage: ./send.sh [input filename] [ip address] [encryption password]"
         exit 1
 fi
 
@@ -14,4 +14,4 @@ openssl dgst -sha256 "$1" | awk '{print $2}' | nc $2 1235
 sleep 1
 echo "Sending file..."
 
-xz -c $1 | openssl enc -aes-256-cbc -base64 -k your_password_here | nc $2 1234
+xz -c $1 | openssl enc -aes-256-cbc -base64 -k "$3" | nc $2 1234
